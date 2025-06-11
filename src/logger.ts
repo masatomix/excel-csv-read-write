4c682d7b000db04309c9d37135362647423b7e82
Masatomi KINO <masatomix@ki-no.org>
2025-06-11 11:10:32 +0900

    Loggeを変更(bunyan → pino) linter、formatter、tsconfig あたりを整理。 yarn から pnpm へ変更。 依存ファイルたちをver.up
    Webで使うかもで、ArrayBufferを受け取るメソッドを追加( excelBuffer2json )
    ```
    <input type="file" accept=".xlsx" onChange={onFileChange} />
    ```
    
    ```
      const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
        const reader = new FileReader();
        reader.onload = async () => {
          try {
            const arrayBuffer = reader.result;
            if (!(arrayBuffer instanceof ArrayBuffer)) {
              console.error("FileReader result is not an ArrayBuffer");
              return;
            }
            const records = await excelBuffer2json(arrayBuffer);
            console.table(records);
            // workbook 操作
            console.log("読み込み成功", records.length, " 件");
          } catch (error) {
            console.error("読み込み失敗", error);
          }
        };
        reader.readAsArrayBuffer(files[0]);
      };
    ```

--- src/logger.ts
+++ src/logger.ts
@@ -1,37 +1,55 @@
-import * as Logger from 'bunyan'
-import * as config from 'config'
+import pino, { Logger } from 'pino'
+import config from 'config'
 
+// const isProd = process.env.NODE_ENV === 'production';
 
-interface LogConfig {
-  logging: Logging[] | undefined
-}
+// モジュールごとのロガーをキャッシュ
+const loggers: Record<string, Logger> = {}
 
-interface Logging {
-  name: string
-}
-
-const isLogConfig = (arg: unknown): arg is LogConfig => {
-  const conf = arg as LogConfig
-
-  return conf.logging !== undefined
-}
-
-export const getLogger = (name: string): Logger => {
-  if (isLogConfig(config)) {
-    const settings = config.logging
-    const target = settings?.find(setting => setting.name === name)
-
-    if (target) {
-      const logger = Logger.createLogger(target)
-      logger.info(`[ ${name} ] のログをココに出力します。`)
-
-      return logger
+interface Transport {
+    target: string
+    options: {
+        translateTime: string
+        ignore: string
+        levelFirst: boolean
     }
-  }
-  const nullLogger = Logger.createLogger({
-    name,
-    level: 'error',
-  })
+}
+interface LoggerConfig {
+    level: string
+    moduleLogLevels: Record<string, pino.LevelWithSilent>
+    transport?: Transport
+}
 
-  return nullLogger
+// デフォルトの設定(このlibrary標準の動き)
+const defaultLoggerOptions: LoggerConfig = {
+    level: 'warn',
+    moduleLogLevels: {
+        FileClassRepository: 'info',
+        GenerateMappingClassUserCase: 'info',
+    },
+    transport: undefined,
+}
+
+// 利用者の設定
+const userLoggerConfig: LoggerConfig | object = config.has('copy-utils-generator-logger')
+    ? config.get('copy-utils-generator-logger')
+    : {}
+const loggerConfig = { ...defaultLoggerOptions, ...userLoggerConfig }
+
+const { transport, moduleLogLevels, level: loglevel } = loggerConfig
+
+/**
+ * モジュール名を指定して Logger を取得。
+ * 初回アクセス時に生成し、以後はキャッシュされたものを返す。
+ */
+export function getLogger(moduleName: string): Logger {
+    if (!loggers[moduleName]) {
+        const level = moduleLogLevels[moduleName] ?? loglevel
+        // console.log(`${moduleName}はなかったので${level}で作る`)
+        loggers[moduleName] = pino({
+            level,
+            transport,
+        }).child({ module: moduleName })
+    }
+    return loggers[moduleName]
 }
